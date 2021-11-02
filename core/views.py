019ca00a4ae9950f3e24eaf1c71aeeaf1affbf98
jeremyakd <jeremiasc@exo.com.ar>
2021-11-02 11:28:45 -0300

    First commit

---
+++ core/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'core/home.html')
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def blog(request):
+    return render(request, 'core/blog.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+def sample(request):
+    return render(request, 'core/sample.html')
+
+def services(request):
+    return render(request, 'core/services.html')
+
+def store(request):
+    return render(request, 'core/store.html')
