8ed17cd0043c93da06c8a4f310bc2fa22ff2b108
jeremyakd <jeremiasc@exo.com.ar>
2021-11-08 20:45:31 -0300

    Create app Blog to manage relations between models

---
+++ blog/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.utils.timezone import now
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Nombre')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Fecha de actualización')
+
+    class Meta:
+        verbose_name = 'categoría'
+        verbose_name_plural = 'categorías'
+
+    def __str__(self) -> str:
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Título")
+    content = models.TextField(verbose_name='Contenido')
+    published = models.DateTimeField(verbose_name='Fecha de publicación',default=now )
+    image = models.ImageField(verbose_name='Imagen', upload_to='blog', null=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category, verbose_name='Categorías')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Fecha de actualización')
+
+    class Meta:
+        verbose_name = 'Posteo'
+        verbose_name_plural = 'Posteos'
+
+    def __str__(self) -> str:
+        return self.title
